3aa454c63ad6be7ad0391fc65039bd2abbfa85cd
alnashar <alnasharm778@gmail.com>
2020-05-06 21:28:33 +0300

    add and modify book

---
+++ src/bookstore/Book.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstore;
+
+/**
+ *
+ * @author elshamey
+ */
+public class Book {
+    int isbn;
+    String titile;
+    String publisher;
+    String publication;
+    float price;
+    String category;
+    int copies;
+    int threshold;
+    
+}
